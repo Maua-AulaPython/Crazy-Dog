1b19248e600869c03c352969859872bddeb11c6f
Alushi <ali.j.anissi@hotmail.com>
2015-06-12 09:15:36 -0300

    trabalho IoT
    trabalho IoT

---
+++ classmodel.py
@@ -0,0 +1,18 @@
+from server import db
+
+
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    measures=db.relationship('Measure')
+        
+
+
+    
+class Measure(db.Model):
+    id_device = db.Column(db.Integer, db.ForeignKey('Device.id'))
+    luminosity = db.Column(db.Float)
+    temperature = db.Column(db.Float)
+    date = db.Column(db.DateTime, primary_key=True)
---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+import json
+from classmodel import *
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite://BancoDeDadosM.db'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return "Nossas rotas sao: '/measures/show', '/measures/new' e '/device/new'"
+
+@app.route('/measures/show', methods=['GET'])
+def measure_list():
+	measures = []
+	for i in Measure.query.all():
+		print i.date, i.id_device,i.luminosity, i.temperature
+		measures.append({'Data': i.date, 'ID:':i.id_device, 'Luminosidade': i.luminosity, 'Temperatura': i.temperature})
+
+	return json.dumps(measures)
+
+
+@app.route('/measures/new', methods=['POST'])
+def measure_new():
+	if not request.json:
+		return jsonify({'status': False})
+
+	p = request.get_json()
+	m = Measure()
+	m.date = datetime.today()
+	m.id_device = p['ID']
+	m.luminosity = p['Luminosidade']
+	m.temperature = p['Temperatura']
+	db.session.add(m)
+	db.session.commit()
+	return jsonify({'status:': True})
+
+
+@app.route('/device/new', methods = ['POST'])
+def estufa_new():
+	if not request.json:
+		return jsonify({'status': False})
+	p = request.get_json()
+	d = Device()
+	d.id = p['ID']
+	d.latitude = p['Latitude']
+	d.longitude = p['Longitude']
+	db.session.add(a)
+	db.session.commit()
+	return jsonify({'status': True})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
